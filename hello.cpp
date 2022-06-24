8c58033f84dd9d6c173f3a90dac71dc89558336b
FeiPF2020 <wpfn218925@163.com>
2022-06-25 01:37:05 +0800

    https://github.com/FeiPF2020/ch1.git

--- hello.cpp
+++ hello.cpp
@@ -2,7 +2,7 @@
  * @Author: FeiPF2020 wpfn218925@163.com
  * @Date: 2022-06-25 01:24:25
  * @LastEditors: FeiPF2020 wpfn218925@163.com
- * @LastEditTime: 2022-06-25 01:26:29
+ * @LastEditTime: 2022-06-25 01:35:05
  * @FilePath: \ch1\hello.cpp
  * 
  * 
@@ -12,5 +12,6 @@ using namespace std;
 
 int main(){
     cout<<"hello,wpf!!!";
+    cout<<"hello,tm";
     return 0;
 }
